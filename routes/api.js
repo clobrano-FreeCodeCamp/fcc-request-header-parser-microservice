e63027b10a283b155c266d941d96aa58db0bc577
Carlo Lobrano <c.lobrano@gmail.com>
2016-05-20 23:53:10 +0200

    Fixed getting client's ip address

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var accepts = require('accepts')
 var parser = require('ua-parser-js');
+var get_ip = require('ipware')().get_ip;
 var router = express.Router();
 
 router.get('/whoami', function(req, res) {
     var json = {};
-    json.ipaddress = req.ip;
+    json.ipaddress = get_ip(req).clientIp;
 
     var acp = accepts(req);
     json.language = acp.language(acp.languages());
